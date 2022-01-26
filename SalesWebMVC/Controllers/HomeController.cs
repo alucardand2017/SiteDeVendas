836b36e01fadcedad52f36ec8fd987e7dc769182
Anderson Silva <anderson_ns5@hotmail.com>
2022-01-26 18:13:57 -0300

    Adicao do Department e inico de sua pagina

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace SalesWebMVC.Controllers
 {
     public class HomeController : Controller
