547e01fca52f1334f75c514090a9a7336b562075
Anderson Silva <anderson_ns5@hotmail.com>
2022-01-27 15:25:33 -0300

    Creação do Seller no formulario e envio para banco de dados.

--- SalesWebMVC/Controllers/Sellers1Controller.cs
+++ SalesWebMVC/Controllers/Sellers1Controller.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
 using SalesWebMVC.Services;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,17 @@ namespace SalesWebMVC.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller )
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index)); //redireciona a ação para o index acima
         }
     }
 }
