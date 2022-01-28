d1fd8687f01f4b9be3bd3d64a0a1070985aa22f7
Anderson Silva <anderson_ns5@hotmail.com>
2022-01-28 16:32:30 -0300

    Validacao dos campos no javascript e no banco de dados em sellerscontroller

--- SalesWebMVC/Controllers/Sellers1Controller.cs
+++ SalesWebMVC/Controllers/Sellers1Controller.cs
@@ -39,6 +39,12 @@ namespace SalesWebMVC.Controllers
         //o framework instancia alem do seller, o department, já que colocamos um private com DepartmentId
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index)); //redireciona a ação para o index acima
         }
@@ -82,6 +88,13 @@ namespace SalesWebMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+                
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mistmach" });
--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -9,16 +9,26 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+        
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(90, MinimumLength = 5, ErrorMessage ="O tamanho do {0} é inválido, deve estar entre {2} e {1}")]
         public string Name { get; set; }
+        
+        [Required(ErrorMessage = "{0} required")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy }")]
         public DateTime BirthDate { get; set; }
+        
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required(ErrorMessage = "{0} required")]
         public double BaseSalary { get; set; }
+        
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
